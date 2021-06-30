5f300e770f3b68212b7bae1e11e110e9c68daf80
eduardodcs <eddcesilva@gmail.com>
2021-06-30 09:11:40 -0300

    Finalizando o curso

---
+++ forumEduardo/src/main/java/br/com/eduardo/forum/controller/TopicosController.java
@@ -0,0 +1,122 @@
+package br.com.eduardo.forum.controller;
+
+import java.net.URI;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import br.com.eduardo.forum.controller.dto.DetalhesDoTopicoDto;
+import br.com.eduardo.forum.controller.dto.TopicoDto;
+import br.com.eduardo.forum.controller.form.AtualizacaoTopicoForm;
+import br.com.eduardo.forum.controller.form.TopicoForm;
+import br.com.eduardo.forum.modelo.Curso;
+import br.com.eduardo.forum.modelo.Topico;
+import br.com.eduardo.forum.repository.CursoRepository;
+import br.com.eduardo.forum.repository.TopicoRepository;
+
+@RestController
+@RequestMapping(value="/topicos")
+public class TopicosController {
+	
+	@Autowired
+	private TopicoRepository topicoRepository;
+	@Autowired
+	private CursoRepository cursoRepository;
+	
+	@GetMapping
+	@Cacheable(value = "listaDeTopicos")
+	public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso,
+			@PageableDefault(sort = "id", direction = Direction.ASC, page = 0, size = 10) Pageable paginacao) {
+		
+		//Podemos colocar um ordenação padrão com o PegeableDefault, inclusive a paginação, assim não precisa vir na requisição
+		
+		//Acrescentar a anotação EnableSpringDataWebSupport no ForumApplication
+		//http://localhost:8080/topicos?page=0&size=10&sort=titulo,asc -> novo modelo de requisicao
+		//http://localhost:8080/topicos?page=0&size=10&sort=titulo,asc&sort=dataCriacao,desc -> pode trazer várias ordenacações
+		
+		//http://localhost:8080/topicos?pagina=0&qtd=1&ordenacao=titulo -> modelo da requisição
+		//Necessário colocar todos os atributos como parametro da classe
+		//Pageable paginacao = PageRequest.of(pagina, qtd, Direction.ASC, ordenacao);
+		
+		if(nomeCurso == null) {
+			Page<Topico> topicos = topicoRepository.findAll(paginacao);
+			return TopicoDto.converter(topicos);
+		} else {
+			Page<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso, paginacao);
+			return TopicoDto.converter(topicos);
+		}
+		
+		
+	}
+	
+	@PostMapping
+	@Transactional
+	@CacheEvict(value = "listaDeTopicos", allEntries = true)
+	public ResponseEntity<TopicoDto> cadastrar(@RequestBody @Valid TopicoForm form, UriComponentsBuilder uriBuilder) {
+		Topico topico = form.converter(cursoRepository);
+		topicoRepository.save(topico);
+		
+		URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
+		return ResponseEntity.created(uri).body(new TopicoDto(topico));
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<DetalhesDoTopicoDto> detalhar(@PathVariable Long id) {
+		Optional<Topico> topico = topicoRepository.findById(id);
+		if (topico.isPresent()) {
+			return ResponseEntity.ok(new DetalhesDoTopicoDto(topico.get()));	
+		} 
+		return ResponseEntity.notFound().build();
+	}
+	
+	@PutMapping("/{id}")
+	@Transactional
+	@CacheEvict(value = "listaDeTopicos", allEntries = true)
+	public ResponseEntity<TopicoDto> atualizar(@PathVariable Long id, @RequestBody @Valid AtualizacaoTopicoForm form){
+		Optional<Topico> optional = topicoRepository.findById(id);
+		if (optional.isPresent()) {
+			Topico topico = form.atualizar(id, topicoRepository);
+			return ResponseEntity.ok(new TopicoDto(topico));
+		} 
+		return ResponseEntity.notFound().build();
+	}
+	
+	@DeleteMapping("/{id}")
+	@Transactional
+	@CacheEvict(value = "listaDeTopicos", allEntries = true)
+	public ResponseEntity<?> remover(@PathVariable Long id){
+		Optional<Topico> optional = topicoRepository.findById(id);
+		if (optional.isPresent()) {
+			topicoRepository.deleteById(id);
+			return ResponseEntity.ok().build();
+		} 
+		return ResponseEntity.notFound().build();
+		
+		
+		
+	}
+}
---
+++ sprint-admin-eduardo/src/test/java/br/com/eduardo/sprintadmineduardo/SprintAdminEduardoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.eduardo.sprintadmineduardo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintAdminEduardoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
